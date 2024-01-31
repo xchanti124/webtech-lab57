30d56839132a512714b7b4dac2cb083649344940
Chantal Ariu <chantalariu227@gmail.com>
2024-01-31 15:35:35 +0100

    Complete assignment 3

---
+++ Assignment 3/script.js
@@ -0,0 +1,384 @@
+const animeForm = document.getElementById("add-anime");
+const resetButton = document.getElementById("reset-archive");
+const formDialog = document.getElementById("form-dialog");
+const formDialogSubmit = document.getElementById("form-dialog-submit");
+const formDialogOpen = document.getElementById("form-dialog-open");
+const formDialogClose = document.getElementById("form-dialog-close");
+const filterSearchApply = document.getElementById("filter-search-apply");
+const archiveUrl = "http://localhost:3000";
+
+// if editingId is null, the form will add
+// if editingId is an item id, the form will update
+let editingId = null;
+let yearFilter = null;
+let searchFilter = null;
+let currentArchive = [];
+
+const sendRequest = async (method, url, data = {}) => {
+    return await fetch(url, {
+        method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+};
+
+const getArchive = async () => {
+    // retrieve everything from the archive (get request to url of archive and return json of that)
+    const response = await fetch(`${archiveUrl}/media`);
+    return await response.json();
+};
+
+const addArchiveItem = async (name, poster, year, genre, description) => {
+    // add archive item by sending post to the api
+    return await sendRequest("POST", `${archiveUrl}/media`, {
+        name,
+        poster,
+        year,
+        genre,
+        description,
+    });
+};
+
+const editArchiveItem = async (
+    itemId,
+    name,
+    poster,
+    year,
+    genre,
+    description
+) => {
+    // edit an archive item by sending post to the api
+    return await sendRequest("PUT", `${archiveUrl}/media/${itemId}`, {
+        name,
+        poster,
+        year,
+        genre,
+        description,
+    });
+};
+
+const deleteArchiveItem = async (itemId) => {
+    return await sendRequest("DELETE", `${archiveUrl}/media/${itemId}`);
+};
+
+const setYearFilter = (year) => {
+    yearFilter = year;
+    render();
+};
+
+const render = async () => {
+    // 1. fetching all the data (we can use getArchive)
+    // 2. removing all of the children in the table, grid, and filter DOM
+    // 3. repopulate the table, grid, and filter DOM
+
+    // ------ 1 ------
+    currentArchive = await getArchive();
+
+    // ------ 2 ------
+    const tableElement = document.getElementById("archive-table");
+    const archiveGridElement = document.getElementById("archive-grid");
+    const filterYearsElement = document.getElementById("filter-years");
+    tableElement.replaceChildren();
+    archiveGridElement.replaceChildren();
+    filterYearsElement.replaceChildren();
+
+    // ------ 3 ------
+    let filteredArchive = [...currentArchive];
+
+    if (yearFilter !== null) {
+        filteredArchive = filteredArchive.filter(
+            (item) => item.year === yearFilter
+        );
+    }
+
+    if (searchFilter !== null) {
+        filteredArchive = filteredArchive.filter(
+            (item) =>
+                item.name.toLowerCase().includes(searchFilter.toLowerCase()) ||
+                item.genre.toLowerCase().includes(searchFilter.toLowerCase())
+        );
+    }
+
+    for (const item of filteredArchive) {
+        ////////////////////////////////////////////////////////////////////////////
+        const rowElement = document.createElement("tr");
+
+        // name column
+        const nameColumnElement = document.createElement("td");
+
+        const nameElement = document.createElement("p");
+        nameElement.innerHTML = item.name;
+
+        nameColumnElement.appendChild(nameElement);
+
+        const tableEditElement = document.createElement("a");
+        tableEditElement.innerHTML = "(edit)";
+        tableEditElement.classList.add("item-action");
+        tableEditElement.href = `#`;
+        tableEditElement.addEventListener("click", (e) => {
+            e.preventDefault();
+            openEditForm(item.id);
+        });
+
+        nameColumnElement.appendChild(tableEditElement);
+
+        const tableDeleteElement = document.createElement("a");
+        tableDeleteElement.innerHTML = "(delete)";
+        tableDeleteElement.classList.add("item-action");
+        tableDeleteElement.href = `#`;
+        tableDeleteElement.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            if (confirm(`Are you sure you want to delete ${item.name}?`)) {
+                deleteArchiveItem(item.id).then(render);
+            }
+        });
+
+        nameColumnElement.appendChild(tableDeleteElement);
+
+        rowElement.appendChild(nameColumnElement);
+
+        // img column
+        const imgColumnElement = document.createElement("td");
+        const figureElement = document.createElement("figure");
+
+        const imgElement = document.createElement("img");
+        imgElement.src = item.poster;
+        imgElement.alt = item.name;
+
+        figureElement.appendChild(imgElement);
+
+        const figureCaptionElement = document.createElement("figcaption");
+        const emTagElement = document.createElement("em");
+        emTagElement.innerHTML = item.name;
+
+        figureCaptionElement.appendChild(emTagElement);
+
+        figureElement.appendChild(figureCaptionElement);
+
+        imgColumnElement.appendChild(figureElement);
+
+        rowElement.appendChild(imgColumnElement);
+
+        // year column
+        const yearColumnElement = document.createElement("td");
+        yearColumnElement.innerHTML = item.year;
+
+        rowElement.appendChild(yearColumnElement);
+
+        // genre column
+        const genreColumnElement = document.createElement("td");
+        const genreArr = item.genre.split(",").map((genre) => genre.trim());
+        const ulElement = document.createElement("ul");
+
+        for (const genre of genreArr) {
+            const liElement = document.createElement("li");
+            liElement.innerHTML = genre;
+            ulElement.appendChild(liElement);
+        }
+
+        genreColumnElement.appendChild(ulElement);
+
+        rowElement.appendChild(genreColumnElement);
+
+        // description column
+        const descriptionElementColumn = document.createElement("td");
+        const descriptionParagraphElement = document.createElement("p");
+        descriptionParagraphElement.innerHTML = item.description;
+
+        descriptionElementColumn.appendChild(descriptionParagraphElement);
+
+        rowElement.appendChild(descriptionElementColumn);
+
+        // adding the row to the table
+        tableElement.appendChild(rowElement);
+
+        ////////////////////////////////////////////////////////////////////////////
+
+        const gridElement = document.createElement("div");
+        gridElement.classList.add("archive-grid");
+
+        // archive image
+        const archiveElement = document.createElement("div");
+        archiveElement.classList.add("archive-image");
+
+        const imageElement = document.createElement("img");
+        imageElement.src = item.poster;
+        imageElement.alt = item.name;
+
+        archiveElement.appendChild(imageElement);
+
+        gridElement.appendChild(archiveElement);
+
+        // archive content
+        const archiveContentElement = document.createElement("div");
+        archiveContentElement.classList.add("archive-content");
+
+        const datePElement = document.createElement("p");
+        datePElement.classList.add("archive-year");
+        datePElement.innerHTML = item.year;
+
+        archiveContentElement.appendChild(datePElement);
+
+        const titlePElement = document.createElement("p");
+        titlePElement.classList.add("archive-title");
+        titlePElement.innerHTML = item.name;
+
+        const gridEditElement = document.createElement("a");
+        gridEditElement.classList.add("edit-non-desktop");
+        gridEditElement.innerHTML = "(edit)";
+        gridEditElement.href = `#`;
+        gridEditElement.addEventListener("click", (e) => {
+            e.preventDefault();
+            openEditForm(item.id);
+        });
+
+        titlePElement.appendChild(gridEditElement);
+
+        archiveContentElement.appendChild(titlePElement);
+
+        const genreUlElement = document.createElement("ul");
+        genreUlElement.classList.add("archive-genre");
+
+        for (const genre of genreArr) {
+            const genreLiElement = document.createElement("li");
+            genreLiElement.innerHTML = genre;
+
+            genreUlElement.appendChild(genreLiElement);
+        }
+
+        archiveContentElement.appendChild(genreUlElement);
+
+        const descriptionPElement = document.createElement("p");
+        descriptionPElement.classList.add("archive-description");
+        descriptionPElement.innerHTML = item.description;
+
+        archiveContentElement.appendChild(descriptionPElement);
+
+        gridElement.appendChild(archiveContentElement);
+
+        // adding the elemnet to the grid
+        archiveGridElement.appendChild(gridElement);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    const animeYears = [
+        ...new Set(currentArchive.map((item) => item.year)),
+    ].sort((x, y) => x - y);
+    animeYears.unshift(null);
+
+    for (const year of animeYears) {
+        if (year === yearFilter) {
+            const yearActivatedLink = document.createElement("a");
+            yearActivatedLink.href = "#";
+            yearActivatedLink.classList.add("filter-link");
+            yearActivatedLink.classList.add("filter-link-activated");
+            yearActivatedLink.innerHTML = year ?? "All years";
+            yearActivatedLink.addEventListener("click", (e) => {
+                e.preventDefault();
+                setYearFilter(null);
+            });
+
+            filterYearsElement.appendChild(yearActivatedLink);
+
+            continue;
+        }
+
+        const yearLink = document.createElement("a");
+        yearLink.innerHTML = year ?? "All years";
+        yearLink.href = "#";
+        yearLink.classList.add("filter-link");
+        yearLink.addEventListener("click", (e) => {
+            e.preventDefault();
+            setYearFilter(year);
+        });
+
+        filterYearsElement.appendChild(yearLink);
+    }
+};
+
+const openEditForm = (itemId) => {
+    // finds an item from currentArchive with id `itemId`
+    const item = currentArchive.find((item) => item.id === itemId);
+
+    if (!item) {
+        return;
+    }
+
+    editingId = itemId;
+
+    document.getElementById("Name").value = item.name;
+    document.getElementById("Poster").value = item.poster;
+    document.getElementById("Year").value = item.year;
+    document.getElementById("Genre").value = item.genre;
+    document.getElementById("Description").value = item.description;
+
+    formDialog.showModal();
+};
+
+const openAddForm = () => {
+    editingId = null;
+
+    document.getElementById("Name").value = "";
+    document.getElementById("Poster").value = "";
+    document.getElementById("Year").value = "";
+    document.getElementById("Genre").value = "";
+    document.getElementById("Description").value = "";
+
+    formDialog.showModal();
+};
+
+const closeForm = () => {
+    formDialog.close();
+};
+
+const submitForm = () => {
+    closeForm();
+
+    let name = document.getElementById("Name").value;
+    let poster = document.getElementById("Poster").value;
+    let year = document.getElementById("Year").value;
+    let genre = document.getElementById("Genre").value;
+    let description = document.getElementById("Description").value;
+
+    if (editingId === null) {
+        // editingId is null; adding new item
+        addArchiveItem(name, poster, year, genre, description).then(render);
+        return;
+    }
+
+    // editingId is not null; editing an existing item
+    editArchiveItem(editingId, name, poster, year, genre, description).then(
+        render
+    );
+};
+
+resetButton.addEventListener("click", () => {
+    if (confirm("Are you sure you want to reset the entire database?")) {
+        resetArchive().then(render);
+    }
+});
+
+formDialogOpen.addEventListener("click", () => {
+    openAddForm();
+});
+
+formDialogClose.addEventListener("click", () => {
+    closeForm();
+});
+
+formDialogSubmit.addEventListener("click", () => {
+    submitForm();
+});
+
+filterSearchApply.addEventListener("click", () => {
+    const searchQuery = document.getElementById("filter-search").value.trim();
+
+    searchFilter = searchQuery === "" ? null : searchQuery;
+    render();
+});
+
+render();
---
+++ Assignment 3/server.js
@@ -0,0 +1,286 @@
+import sqlite from "sqlite3";
+import express from "express";
+
+sqlite.verbose();
+
+const app = express();
+const db = my_database("./media.db");
+
+const resp500 = { error: "Internal server error" };
+const resp404 = { error: "Not found" };
+const respSuccess = { message: "Operation successful" };
+
+app.use(express.json());
+
+// This disables the CORS verification (source for the fix: https://gist.github.com/chernysh2909/6bb5b089c2cb6f8dcb7185f383c30107)
+// Source for the method fix https://stackoverflow.com/questions/44914330/method-put-is-not-allowed-by-access-control-allow-methods-in-preflight-response
+app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.use((err, _, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+        console.error(err);
+
+        res.set("Content-Type", "application/json");
+        res.status(400);
+        res.send({ error: err.message });
+        return;
+    }
+
+    next();
+});
+
+app.get("/media", (_, res) => {
+    db.all(`SELECT * FROM media`, (err, rows) => {
+        res.set("Content-Type", "application/json");
+
+        if (err) {
+            res.status(500);
+            res.json(resp500);
+            return;
+        }
+
+        res.status(200);
+        res.json(rows);
+    });
+});
+
+// CREATE
+app.post("/media", (req, res) => {
+    res.set("Content-Type", "application/json");
+
+    const allowedFields = ["name", "year", "genre", "poster", "description"];
+
+    for (const field of allowedFields) {
+        if (
+            req.body[field] === undefined ||
+            typeof req.body[field] !== "string"
+        ) {
+            res.status(400);
+            res.json({
+                error: `Field "${field}" should be present with a type of string`,
+            });
+            return;
+        }
+    }
+
+    db.run(
+        `INSERT INTO media (name, year, genre, poster, description) VALUES (?, ?, ?, ?, ?)`,
+        [
+            req.body.name,
+            req.body.year,
+            req.body.genre,
+            req.body.poster,
+            req.body.description,
+        ],
+        function (err) {
+            if (err) {
+                res.status(500);
+                res.json(resp500);
+                return;
+            }
+
+            res.status(200);
+            res.json({ id: this.lastID });
+        }
+    );
+});
+
+// READ
+app.get("/media/:id", (req, res) => {
+    db.get(
+        "SELECT * FROM media WHERE id = ?",
+        [Number(req.params.id)],
+        (err, row) => {
+            res.set("Content-Type", "application/json");
+
+            if (err) {
+                res.status(500);
+                res.json(resp500);
+                return;
+            }
+
+            if (!row) {
+                res.status(404);
+                res.json(resp404);
+                return;
+            }
+
+            res.status(200);
+            res.json(row);
+        }
+    );
+});
+
+// UPDATE
+app.put("/media/:id", (req, res) => {
+    res.set("Content-Type", "application/json");
+
+    // we should only considered known fields
+    // all fields are optional, if a field is undefined, then that media field will not get changed in the db
+    // all fields that are provided and are in allow list should be strings
+    // all fields that are not in the allowlist will get ignored
+
+    const allowedFields = ["name", "year", "genre", "poster", "description"];
+    const changes = {};
+
+    for (const field of allowedFields) {
+        if (req.body[field] !== undefined) {
+            if (typeof req.body[field] !== "string") {
+                res.status(400);
+                res.json({ error: `Field "${field}" should be a string` });
+                return;
+            }
+
+            changes[field] = req.body[field];
+        }
+    }
+
+    db.get(
+        `SELECT * FROM media WHERE id = ?`,
+        [Number(req.params.id)],
+        (err, row) => {
+            if (err) {
+                res.status(500);
+                res.json(resp500);
+                return;
+            }
+
+            if (!row) {
+                res.status(404);
+                res.json(resp404);
+                return;
+            }
+
+            const finalChanges = { ...row, ...changes };
+
+            db.run(
+                "UPDATE media SET name = ?, year = ?, genre = ?, poster = ?, description = ? WHERE id = ?",
+                [
+                    finalChanges.name,
+                    finalChanges.year,
+                    finalChanges.genre,
+                    finalChanges.poster,
+                    finalChanges.description,
+                    finalChanges.id,
+                ],
+                (err) => {
+                    if (err) {
+                        res.status(500);
+                        res.json(resp500);
+                        return;
+                    }
+
+                    res.status(200);
+                    res.json(respSuccess);
+                }
+            );
+        }
+    );
+});
+
+// DELETE
+app.delete("/media/:id", (req, res) => {
+    db.get(
+        "SELECT * FROM media WHERE id = ?",
+        [Number(req.params.id)],
+        (err, row) => {
+            res.set("Content-Type", "application/json");
+
+            if (err) {
+                res.status(500);
+                res.json(resp500);
+                return;
+            }
+
+            if (!row) {
+                res.status(404);
+                res.json(resp404);
+                return;
+            }
+
+            db.run(
+                "DELETE FROM media WHERE id = ?",
+                [Number(req.params.id)],
+                (err) => {
+                    if (err) {
+                        res.status(500);
+                        res.json(resp500);
+                        return;
+                    }
+
+                    res.status(200);
+                    res.json(respSuccess);
+                }
+            );
+        }
+    );
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000...");
+});
+
+function my_database(filename) {
+    // Conncect to db by opening filename, create filename if it does not exist:
+    const db = new sqlite.Database(filename, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("Connected to the media database.");
+    });
+
+    // Create our media table if it does not exist already:
+    db.serialize(() => {
+        db.run(`
+              CREATE TABLE IF NOT EXISTS media
+               (
+                      id INTEGER PRIMARY KEY,
+                      name CHAR(100) NOT NULL,
+                      year CHAR(100) NOT NULL,
+                      genre CHAR(256) NOT NULL,
+                      poster char(2048) NOT NULL,
+                      description CHAR(1024) NOT NULL
+           )
+          `);
+        db.all(`select count(*) as count from media`, (_, result) => {
+            if (result[0].count == 0) {
+                db.run(
+                    `INSERT INTO media (name, year, genre, poster, description) VALUES (?, ?, ?, ?, ?)`,
+                    [
+                        "Arcane",
+                        "2021",
+                        "animation, action, adventure, tv-show",
+                        "https://www.nerdpool.it/wp-content/uploads/2021/11/poster-arcane.jpg",
+                        "Set in Utopian Piltover and the oppressed underground of Zaun, the story follows the origins of two iconic League Of Legends champions and the power that will tear them apart.",
+                    ]
+                );
+                db.run(
+                    `INSERT INTO media (name, year, genre, poster, description) VALUES (?, ?, ?, ?, ?)`,
+                    [
+                        "Celeste",
+                        "2018",
+                        "platformer, video-game",
+                        "https://upload.wikimedia.org/wikipedia/commons/0/0f/Celeste_box_art_full.png",
+                        "Celeste is a critically acclaimed two-dimensional platform game developed by Maddy Makes Games. The player controls Madeline, a young woman who sets out to climb Celeste Mountain. The game features tight controls, challenging levels, and a touching story about overcoming personal demons.",
+                    ]
+                );
+                console.log("Inserted dummy photo entry into empty database");
+            } else {
+                console.log(
+                    "Database already contains",
+                    result[0].count,
+                    " item(s) at startup."
+                );
+            }
+        });
+    });
+    return db;
+}
